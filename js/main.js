0d03605580701dcdd948be0993c90bf856b9213f
Rayhan Yulanda <rayhanyulanda@gmail.com>
2021-11-27 19:42:06 +0700

    Fix | Hide burger menu on mobile

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@
 	var offcanvasMenu = function() {
 
 		$('#page').prepend('<div id="fh5co-offcanvas" />');
-		$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
+		//$('#page').prepend('<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle fh5co-nav-white"><i></i></a>');
 		var clone1 = $('.menu-1 > ul').clone();
 		$('#fh5co-offcanvas').append(clone1);
 		var clone2 = $('.menu-2 > ul').clone();
